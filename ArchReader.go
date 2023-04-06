b79fdcb572daf415cf6c1644c4f5a2988a664c3e
Linkort <khaidarshin.artur@gmail.com>
2023-04-07 01:34:33 +0500

    Добавлен Yaml-файл конфигурации - Добавлен yaml файл с настройками, теперь при пропуске ввода данных для   запроса архива-применятся настройки с конфиг.файла - При пропуске ввода №треб.архива - контроллер ответит последним архивов - В Yaml добавлены данные для расшифровки шапки архива - Добавлен преварительный разбор шапки архива - Программа не завершается после рабора ответа - программа запросит   пользователя ввести след. требуемый архив

--- ArchReader.go
+++ ArchReader.go
@@ -4,19 +4,42 @@ import (
 	"bytes"
 	"encoding/binary"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
 	"time"
+
+	"gopkg.in/yaml.v3"
 )
 
-func errfunc(err error) {
-	if err != nil {
-		log.Fatal(err.Error())
-	}
+type configfile struct {
+	Com        string   `yaml:"defs_com"`
+	Baud       string   `yaml:"defs_baud"`
+	Plc        uint16   `yaml:"defs_plcaddress"`
+	TitleBytes []int    `yaml:"R7title_bytecount"`
+	TitleSpec  []string `yaml:"R7title_comment"`
 }
 
-func Read_response(file *os.File) (res [76]byte) {
+var Conf configfile
+
+func yamlRead() { //Чтение файла настроек и архивов
+	yfile, err := ioutil.ReadFile("configs.yml")
+	if err != nil {
+		fmt.Println(err.Error())
+		fmt.Println("Файл конфигурации не найден")
+		return
+	}
+	if err = yaml.Unmarshal(yfile, &Conf); err != nil {
+		fmt.Println(err.Error())
+		fmt.Println("Ошибка в структуре файла конфигурации")
+		return
+	}
+	//	fmt.Print(R7)
+	fmt.Println("Файл настроек прочитан")
+}
+
+func Read_response(file *os.File) (res [76]byte) { //Чтение ответа
 	ch := make(chan [76]byte)
 	go func() {
 		var arr [76]byte
@@ -32,12 +55,25 @@ func Read_response(file *os.File) (res [76]byte) {
 
 	select {
 	case res := <-ch:
+		//Проверка на валидность пакета
 		fmt.Println("Ответ получен")
 		return res
 	case <-time.After(time.Second * 5):
 		fmt.Println("Timeout")
 	}
 	return res
+}
+
+func request(sl uint16, arch uint32, file *os.File) { //Формирование и отправка запроса
+	request_buff := bytes.Buffer{}
+	request_buff.Write([]byte{0x11, 0xf0, 0x0e, 0x00, 0x52, 0x37})
+	binary.Write(&request_buff, binary.LittleEndian, sl)
+	binary.Write(&request_buff, binary.LittleEndian, arch)
+	binary.Write(&request_buff, binary.LittleEndian, CRC(request_buff.Bytes()))
+	// Отправка запроса
+	file.Write([]byte{0x7e}) //Пилот протокола RTM
+	request_buff.WriteTo(file)
+	file.Write([]byte{0x7e}) //Пилот протокола RTM
 }
 
 func CRC(buff []byte) uint16 { //RTM CRC (CRC-16/XMODEM)
@@ -55,33 +91,47 @@ func CRC(buff []byte) uint16 { //RTM CRC (CRC-16/XMODEM)
 	return crc
 }
 
-func main() {
-	var comport string
-	var needArch uint32
-	var slave uint16
-	fmt.Print("Введите Com-порт: ")
-	fmt.Scan(&comport)
-	fmt.Print("Введите modbus адрес ПЛК: ")
-	fmt.Scan(&slave)
-	fmt.Print("Введите № требуемого архива: ")
-	fmt.Scan(&needArch)
+func errfunc(err error) { //вывод ошибки в лог при наличии
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
 
+func main() {
+	yamlRead() //Чтение YAML
+	//Ввод значений
+	var needArch uint32
+	fmt.Print("Введите Com-порт: ")
+	fmt.Scanln(&Conf.Com)
+	fmt.Print("Введите modbus адрес ПЛК: ")
+	fmt.Scanln(&Conf.Plc)
 	// открытие порта
-	exec.Command("mode", "com"+comport+":19200,N,8,1").Run() //Настройка порта
-	file, err := os.OpenFile("COM"+comport, os.O_RDWR, 0700)
+	exec.Command("mode", "com"+Conf.Com+":"+Conf.Baud+",N,8,1").Run() //Настройка порта
+	file, err := os.OpenFile("COM"+Conf.Com, os.O_RDWR, 0700)
 	errfunc(err)
 	defer file.Close()
-	// Формирование запроса
-	request_buff := bytes.Buffer{}
-	request_buff.Write([]byte{0x11, 0xf0, 0x0e, 0x00, 0x52, 0x37})
-	binary.Write(&request_buff, binary.LittleEndian, slave)
-	binary.Write(&request_buff, binary.LittleEndian, needArch)
-	binary.Write(&request_buff, binary.LittleEndian, CRC(request_buff.Bytes()))
-	// Отправка запроса
-	file.Write([]byte{0x7e}) //Пилот протокола RTM
-	request_buff.WriteTo(file)
-	file.Write([]byte{0x7e}) //Пилот протокола RTM
-	// Чтение ответа
-	RTMresponse := Read_response(file)
-	fmt.Print(RTMresponse)
+	for {
+		fmt.Print("Введите № требуемого архива: ")
+		if fmt.Scanln(&needArch); needArch == 0 {
+			needArch = 1073741824 //Запрос последнего архива. Если запрашиваемый №архива > №последнего - ПЛК вышлет последний.
+		}
+		// Формирование и отправка запроса
+		request(Conf.Plc, needArch, file)
+		// Чтение ответа
+		RTMresponse := Read_response(file)
+		fmt.Println(RTMresponse)
+		//Разбор шаки архива
+		var t = 9 //адрес начала данных в ответе
+		for i, count := range Conf.TitleBytes {
+			for j := 0; j < count; j++ {
+				if j == 0 {
+					fmt.Println(RTMresponse[t], "---", Conf.TitleBytes[i], "---", Conf.TitleSpec[i])
+				} else {
+					fmt.Println(RTMresponse[t])
+				}
+				t++
+			}
+		}
+	}
+
 }
